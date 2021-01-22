12fa4065033ff15be0812d0598d8e0c7b9d28b51
r2ast <rahul.angulardev@gmail.com>
2021-01-22 14:47:13 +0530

    google charts in angular with phpApi withmysql db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
+import {pipe} from 'rxjs';
 import { Component } from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chat-app';
+  //Declare variable to store API values
+  cartkey = [];
+  myType: any;
+  myType1: any;
+  myData:any;
+  myOptions:any;
+  title:any;
+
+    
+  constructor(private http: HttpClient) {
+  
+  this.http.get('http://localhost:8080/chartApi/mypage.php').subscribe((data:any) => {
+  //Convert json to array  
+    alert(JSON.stringify(data));
+  this.cartkey = data.map(function(item:any) {
+  //Merge array
+  var mixarrayy = (Object.keys(item)).concat(Object.values(item));
+  return mixarrayy;
+  
+  });
+  setTimeout(() => {
+    this.myType = 'PieChart';
+    this.myType1 = 'ColumnChart';
+  this.myData = this.cartkey;
+  this.myOptions = {
+  colors: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
+  is3D: true
+  };
+  }, 2500);
+  });
+  }
 }
